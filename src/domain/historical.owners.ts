034cb37030356b589e4aa48c3ae23dbcfdba3ef7
Rodrigo Coura <rodrigoc@corebridge.net>
2021-09-17 14:59:40 -0300

    Adding from on historial owners

--- src/domain/historical.owners.ts
+++ src/domain/historical.owners.ts
@@ -2,6 +2,7 @@ import { model, Schema, Model, Document } from 'mongoose';
 
 export interface IHistoricalOwners {
   token: number;
+  from: string;
   owner: string;
   contract: string;
   createdAt: Date;
@@ -9,6 +10,7 @@ export interface IHistoricalOwners {
 
 export interface HistoricalOwnersDocument extends Document {
   token: IHistoricalOwners['token'];
+  from: IHistoricalOwners['from'];
   owner: IHistoricalOwners['owner'];
   contract: IHistoricalOwners['contract'];
   createdAt: IHistoricalOwners['createdAt'];
@@ -16,6 +18,7 @@ export interface HistoricalOwnersDocument extends Document {
 
 export const HOwnersSchema: Schema = new Schema({
   token: { type: Number },
+  from: { type: String },
   owner: { type: String },
   contract: { type: String },
   createdAt: { type: Date },
