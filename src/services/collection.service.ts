3fc4c600b16d177b38d89f2217e638070f822ea0
Rodrigo Cour <rrcoura@gmail.com>
2022-04-25 13:07:22 -0300

    Fixing external NFT duplication

--- src/services/collection.service.ts
+++ src/services/collection.service.ts
@@ -75,14 +75,14 @@ export default class CollectionService extends BaseCRUDService<ICollection> {
 
   async getAsync(id: string): Promise<Result<ICollection>> {
     const data = await CollectionContext.findById(id);
-    if(data)
+    if (data)
       return Result.success<ICollection>(null, data as ICollection);
     return Result.fail<ICollection>('Collection not found!', null, 404);
   }
 
   async getByAddressAsync(title: string): Promise<Result<ICollection>> {
     const data = await CollectionContext.findOne({ owner: title.toLowerCase() });
-    if(data)
+    if (data)
       return Result.success<ICollection>(null, data as ICollection);
     return Result.fail<ICollection>('Collection not found!', null, 404);
   }
@@ -103,7 +103,7 @@ export default class CollectionService extends BaseCRUDService<ICollection> {
       account: sign.account
     });
 
-    if(result) {
+    if (result) {
       return await this.updateAsync((result as CollectionDocument)._id, {
         avatar: request.data.avatar,
         description: request.data.description,
@@ -112,7 +112,7 @@ export default class CollectionService extends BaseCRUDService<ICollection> {
       });
     }
 
-    return responseResult;    
+    return responseResult;
   }
 
   async approveCollection(request: ICollectionApproveRequest, id: number, disapprove: boolean) {
@@ -129,14 +129,14 @@ export default class CollectionService extends BaseCRUDService<ICollection> {
       blockchainId: id
     });
 
-    if(result && id.toString() === request.data.collectionId.toString()) {
+    if (result && id.toString() === request.data.collectionId.toString()) {
       return await this.updateAsync((result as CollectionDocument)._id, {
         show: !disapprove,
         approvedBy: request.data.approvedBy
       });
     }
 
-    return responseResult;    
+    return responseResult;
   }
 
   async _fillUser(user: any, account: string, role: string) {
@@ -144,7 +144,7 @@ export default class CollectionService extends BaseCRUDService<ICollection> {
 
     user.role = role;
 
-    if(existingAccount) {
+    if (existingAccount) {
       user.name = existingAccount.name || account;
       user.avatar = existingAccount.avatar;
       user.email = existingAccount.email;
@@ -165,138 +165,137 @@ export default class CollectionService extends BaseCRUDService<ICollection> {
     if (!(await signService.validate<ICollectionExternalNFTRequestSignData>(request, request.data, 'collection_creation')).isValid)
       throw new Exception(400, "INVALID_SIGN", "The sent data is not valid!", null);
 
-    if (!(await this._checkUniqueness(request.data))) {
-      let result : Result<ICollection> | null = await this.getByAddressAsync(request.data.address);
-      
-      if (result.success && result.data && result.data.owner) {
-        responseResult = result;
-      } else {
-        const collectionCreated = await this.createAsync(<ICollection>{
-          title: request.data.name,
-          namelc: request.data.name.toLowerCase(),
-          show: false,
-          description: request.data.name,
-          isCustom: true,
-          owner: request.data.address,
-          account: request.data.account
-        });
+    let collectionCreated: Result<ICollection> | null = await this.getByAddressAsync(request.data.address);
 
-        if(collectionCreated.success) {
-          const nftsAndImages = await Promise.all(request.data.nfts.map(async a => {
-            const newNFT = {
-              //HARD CODED ARTIST
-              artist: {
-                name: "Custom",
-                address: request.data.address.toLowerCase(),
-                website: "https://www.algopainter.art",
-                twitter: "https://www.twitter.com/algopainter",
-                github: "https://github.com/algopainter",
-                instagram: "https://www.instagram.com/algopainter",
-                avatar: "",
-              },
-              collectionHash: request.data.address.toLowerCase(),
-              pirs: { creatorRate: null, investorRate: null },
-              isRecovered: false,
-              supplyIndex: a.id,
-              contractAddress: request.data.address.toLowerCase(),
-              description: a.description,
-              mintedBy: request.data.account.toLowerCase(),
-              owner: request.data.account.toLowerCase(),
-              name: a.name || "N/A",
-              collectionName: request.data.name,
-              image: a.image,
-              previewImage: a.image,
-              rawImage: a.image,
-              initialPrice: {
-                amount: 0,
-                tokenSymbol: "",
-                tokenAddress: "",
-              },
-              parameters: a.params,
-              descriptor: a.descriptor,
-            };
+    if (!collectionCreated.success && !collectionCreated.data) {
+      collectionCreated = await this.createAsync(<ICollection>{
+        title: request.data.name,
+        namelc: request.data.name.toLowerCase(),
+        show: false,
+        description: request.data.name,
+        isCustom: true,
+        owner: request.data.address.toLowerCase(),
+        account: request.data.account.toLowerCase()
+      });
+    }
 
-            const createdNFT = await NFTContext.create(newNFT);
-
-            const newImg = {
-              title: newNFT.name,
-              likes: 0,
-              users: [] as any[],
-              pirs: 0,
-              creator: (newNFT.mintedBy || newNFT.artist.address).toLowerCase(),
-              owner: (newNFT.mintedBy || newNFT.artist.address).toLowerCase(),
-              description: newNFT.description,
-              collectionName: newNFT.collectionName,
-              collectionOwner: newNFT.contractAddress.toLowerCase(),
-              collectionHash: newNFT.collectionHash.toLowerCase(),
-              createdAt: new Date(),
-              updatedAt: new Date(),
-              nft: {
-                _id: createdNFT._id.valueOf(),
-                index: parseInt(newNFT.supplyIndex),
-                previewImage: newNFT.image,
-                image: newNFT.image,
-                rawImage: newNFT.image,
-                parameters: newNFT.parameters
-              },
-              tags: [],
-              likers: [],
-              initialPrice: newNFT.initialPrice
-            };
-
-            let imgOwner = {
-              name: newImg.owner.toLowerCase(),
-              email: '',
-              avatar: '',
-              account: newImg.owner.toLowerCase(),
-              webSite: '',
-              twitter: '',
-              customProfile: '',
-              instagram: '',
-              type: 'user',
-              role: 'owner'
-            };
-        
-            let imgCreator = {
-              name: newImg.creator.toLowerCase(),
-              avatar: '',
-              email: '',
-              account: newImg.creator.toLowerCase(),
-              webSite: '',
-              twitter: '',
-              customProfile: '',
-              instagram: '',
-              type: 'user',
-              role: 'creator'
-            };
-        
-            await this._fillUser(imgOwner, newImg.owner, 'owner');
-        
-            if(newImg.creator != newImg.owner) {
-              await this._fillUser(imgCreator, newImg.creator, 'creator');
-            } else {
-              imgCreator = { ...imgOwner };
-              imgCreator.role = 'creator';
-            }
-        
-            newImg.users.push(imgOwner);
-            newImg.users.push(imgCreator);
-        
-            await ImageContext.create(newImg);
-
-            return {
-              nft: newNFT,
-              image: newImg 
-            }
-          }));
-
-          if(nftsAndImages && nftsAndImages.length) {
-            responseResult = Result.success<ICollection>(null, null)
+    if (collectionCreated.success) {
+      const nftsAndImages = await Promise.all(request.data.nfts.map(async a => {
+        try {
+          const newNFT = {
+            //HARD CODED ARTIST
+            artist: {
+              name: "Custom",
+              address: request.data.address.toLowerCase(),
+              website: "https://www.algopainter.art",
+              twitter: "https://www.twitter.com/algopainter",
+              github: "https://github.com/algopainter",
+              instagram: "https://www.instagram.com/algopainter",
+              avatar: "",
+            },
+            collectionHash: request.data.address.toLowerCase(),
+            pirs: { creatorRate: null, investorRate: null },
+            isRecovered: false,
+            supplyIndex: a.id,
+            contractAddress: request.data.address.toLowerCase(),
+            description: a.description,
+            mintedBy: request.data.account.toLowerCase(),
+            owner: request.data.account.toLowerCase(),
+            name: a.name || "N/A",
+            collectionName: request.data.name,
+            image: a.image,
+            previewImage: a.image,
+            rawImage: a.image,
+            initialPrice: {
+              amount: 0,
+              tokenSymbol: "",
+              tokenAddress: "",
+            },
+            parameters: a.params,
+            descriptor: a.descriptor,
+          };
+  
+          const createdNFT = await NFTContext.create(newNFT);
+  
+          const newImg = {
+            title: newNFT.name,
+            likes: 0,
+            users: [] as any[],
+            pirs: 0,
+            creator: (newNFT.mintedBy || newNFT.artist.address).toLowerCase(),
+            owner: (newNFT.mintedBy || newNFT.artist.address).toLowerCase(),
+            description: newNFT.description,
+            collectionName: newNFT.collectionName,
+            collectionOwner: newNFT.contractAddress.toLowerCase(),
+            collectionHash: newNFT.collectionHash.toLowerCase(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            nft: {
+              _id: createdNFT._id.valueOf(),
+              index: parseInt(newNFT.supplyIndex),
+              previewImage: newNFT.image,
+              image: newNFT.image,
+              rawImage: newNFT.image,
+              parameters: newNFT.parameters
+            },
+            tags: [],
+            likers: [],
+            initialPrice: newNFT.initialPrice
+          };
+  
+          let imgOwner = {
+            name: newImg.owner.toLowerCase(),
+            email: '',
+            avatar: '',
+            account: newImg.owner.toLowerCase(),
+            webSite: '',
+            twitter: '',
+            customProfile: '',
+            instagram: '',
+            type: 'user',
+            role: 'owner'
+          };
+  
+          let imgCreator = {
+            name: newImg.creator.toLowerCase(),
+            avatar: '',
+            email: '',
+            account: newImg.creator.toLowerCase(),
+            webSite: '',
+            twitter: '',
+            customProfile: '',
+            instagram: '',
+            type: 'user',
+            role: 'creator'
+          };
+  
+          await this._fillUser(imgOwner, newImg.owner, 'owner');
+  
+          if (newImg.creator != newImg.owner) {
+            await this._fillUser(imgCreator, newImg.creator, 'creator');
+          } else {
+            imgCreator = { ...imgOwner };
+            imgCreator.role = 'creator';
           }
+  
+          newImg.users.push(imgOwner);
+          newImg.users.push(imgCreator);
+  
+          await ImageContext.create(newImg);
+  
+          return {
+            nft: newNFT,
+            image: newImg
+          }
+        } catch(e) {
+          console.error(e);
+          return null;
         }
+      }));
+
+      if (nftsAndImages && nftsAndImages.length) {
+        responseResult = Result.success<ICollection>(null, null)
       }
-    } else {
-      responseResult = Result.custom<ICollection>(false, "The data sent is not unique!", null, 409, 390);
     }
 
     return responseResult;
